b8adfc4b937c52c05a061192b1952c7cd978fe0f
CHODISETTI VEERA SUBRAHMANYAM <83783005+subrahmanyam-hub@users.noreply.github.com>
2021-11-15 18:50:04 +0530

    Update app.py

--- project/app.py
+++ project/app.py
@@ -7,7 +7,7 @@ import re
 
 # CSS Design
 from load_css import local_css
-local_css(r"style.css")
+#local_css(r"style.css")
 
 # For NLP & Preprocessing
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
